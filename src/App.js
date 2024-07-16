6be85ccf400528ddd49fd1967a32eaf8fab6daf6
Swadeep <swadeep.p@cisinlabs.com>
2024-07-16 13:25:45 +0530

    Added 3-level loader

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ const App = () => (
   <>
     <Loader height="50px" width="50px" bgColor="black" loaderColor="red" transitionColour={{ loader: "white", background: "orange" }}><div /></Loader>
     <Loader height="100px" width="100px" bgColor="orange" loaderColor="white" transitionColour={{ loader: "red", background: "black" }}><div /></Loader>
+    <Loader height="150px" width="150px" bgColor="pink" loaderColor="cyan" transitionColour={{ loader: "mediumseagreen", background: "white" }}><div /></Loader>
   </>
 );
 export default App;
